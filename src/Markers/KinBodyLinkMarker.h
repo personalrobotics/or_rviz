32644e19559c9ddadb8b095b0b6c4b84b4cb820a
mklingen <mklingen@ash.personalrobotics.ri.cmu.edu>
2014-12-18 17:36:05 -0500

    merging with interactive_marker

---
+++ src/Markers/KinBodyLinkMarker.h
@@ -0,0 +1,44 @@
+#ifndef KINBODYLINKMARKER_H_
+#define KINBODYLINKMARKER_H_
+#include <interactive_markers/interactive_marker_server.h>
+#include "LinkMarker.h"
+
+namespace or_interactivemarker
+{
+
+    class KinBodyLinkMarker;
+    typedef boost::shared_ptr<KinBodyLinkMarker> KinBodyLinkMarkerPtr;
+
+    class KinBodyLinkMarker: public LinkMarker
+    {
+        public:
+            KinBodyLinkMarker(boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server, OpenRAVE::KinBody::LinkPtr link);
+
+            interactive_markers::MenuHandler &menu_handler();
+
+            virtual bool EnvironmentSync();
+            void UpdateMenu();
+
+        private:
+            typedef interactive_markers::MenuHandler MenuHandler;
+
+            bool menu_changed_;
+            std::vector<visualization_msgs::MenuEntry> menu_entries_;
+            MenuHandler menu_handler_;
+            MenuHandler::EntryHandle menu_link_;
+            MenuHandler::EntryHandle menu_visible_;
+            MenuHandler::EntryHandle menu_enabled_;
+            MenuHandler::EntryHandle menu_geom_;
+            MenuHandler::EntryHandle menu_geom_visual_;
+            MenuHandler::EntryHandle menu_geom_collision_;
+            MenuHandler::EntryHandle menu_geom_both_;
+            MenuHandler::EntryHandle menu_groups_;
+            boost::unordered_map<std::string, MenuHandler::EntryHandle> menu_groups_entries_;
+
+            void CreateMenu();
+            void MenuCallback(visualization_msgs::InteractiveMarkerFeedbackConstPtr const &feedback);
+    };
+
+}
+
+#endif
---
+++ src/or_conversions.cpp
@@ -0,0 +1,116 @@
+#include "or_conversions.h"
+
+using OpenRAVE::dReal;
+using OpenRAVE::RaveVector;
+using OpenRAVE::RaveTransform;
+using std_msgs::ColorRGBA;
+using geometry_msgs::Vector3;
+using geometry_msgs::Pose;
+using geometry_msgs::Point;
+using geometry_msgs::Quaternion;
+
+namespace or_interactivemarker {
+
+/*
+ * OpenRAVE to ROS
+ */
+template <class Scalar>
+ColorRGBA toROSColor(RaveVector<Scalar> const &or_color)
+{
+    ColorRGBA color;
+    color.r = or_color.x;
+    color.g = or_color.y;
+    color.b = or_color.z;
+    color.a = or_color.w;
+    return color;
+}
+
+template <class Scalar>
+Vector3 toROSVector(RaveVector<Scalar> const &or_vector)
+{
+    Vector3 v;
+    v.x = or_vector.x;
+    v.y = or_vector.y;
+    v.z = or_vector.z;
+    return v;
+}
+
+template <class Scalar>
+Pose toROSPose(RaveTransform<Scalar> const &or_pose)
+{
+    Pose pose;
+    pose.position = toROSPoint<>(or_pose.trans);
+    pose.orientation = toROSQuaternion<>(or_pose.rot);
+    return pose;
+}
+
+template <class Scalar>
+Point toROSPoint(RaveVector<Scalar> const &or_point)
+{
+    Point point;
+    point.x = or_point.x;
+    point.y = or_point.y;
+    point.z = or_point.z;
+    return point;
+}
+
+template <class Scalar>
+Quaternion toROSQuaternion(RaveVector<Scalar> const &or_quat)
+{
+    Quaternion quaternion;
+    quaternion.w = or_quat[0];
+    quaternion.x = or_quat[1];
+    quaternion.y = or_quat[2];
+    quaternion.z = or_quat[3];
+    return quaternion;
+}
+
+/*
+ * ROS to OpenRAVE
+ */
+template <class Scalar>
+RaveVector<Scalar> toORPoint(Point const &point)
+{
+    return RaveVector<Scalar>(point.x, point.y, point.z);
+}
+
+template <class Scalar>
+RaveVector<Scalar> toORQuaternion(Quaternion const &quat)
+{
+    RaveVector<Scalar> or_quat;
+    or_quat[0] = quat.w;
+    or_quat[1] = quat.x;
+    or_quat[2] = quat.y;
+    or_quat[3] = quat.z;
+    return or_quat;
+}
+
+template <class Scalar>
+RaveTransform<Scalar> toORPose(Pose const &pose)
+{
+    RaveTransform<Scalar> or_transform;
+    or_transform.trans = toORPoint<Scalar>(pose.position);
+    or_transform.rot = toORQuaternion<Scalar>(pose.orientation);
+    return or_transform;
+}
+
+// Explicit instantiations.
+template ColorRGBA toROSColor<float>(RaveVector<float> const &color);
+template Vector3 toROSVector<float>(RaveVector<float> const &or_vector);
+template Pose toROSPose<float>(RaveTransform<float> const &or_pose);
+template Point toROSPoint<float>(RaveVector<float> const &or_point);
+template Quaternion toROSQuaternion<float>(RaveVector<float> const &or_point);
+template RaveVector<float> toORPoint(Point const &point);
+template RaveVector<float> toORQuaternion(Quaternion const &quat);
+template RaveTransform<float> toORPose(Pose const &pose);
+
+template ColorRGBA toROSColor<double>(RaveVector<double> const &color);
+template Vector3 toROSVector<double>(RaveVector<double> const &or_vector);
+template Pose toROSPose<double>(RaveTransform<double> const &or_pose);
+template Point toROSPoint<double>(RaveVector<double> const &or_point);
+template Quaternion toROSQuaternion<double>(RaveVector<double> const &or_point);
+template RaveVector<double> toORPoint(Point const &point);
+template RaveVector<double> toORQuaternion(Quaternion const &quat);
+template RaveTransform<double> toORPose(Pose const &pose);
+
+}
