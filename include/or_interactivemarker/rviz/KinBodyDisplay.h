2c415a2e5b6860eddb9519480b3d5ac017d275b3
Michael Koval <mkoval@cs.cmu.edu>
2015-01-09 19:01:36 -0500

    Revert "Removed un-used native rendering code."
    This reverts commit ad0ab5fe3642ae999d9846382d5fc15fcb92afea.

---
+++ include/or_interactivemarker/rviz/KinBodyDisplay.h
@@ -0,0 +1,44 @@
+/*
+ * KinBodyDisplay.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: mklingen
+ */
+
+#ifndef KINBODYDISPLAY_H_
+#define KINBODYDISPLAY_H_
+
+#include <openrave-core.h>
+
+#include <rviz/display.h>
+#include "KinBodyVisual.h"
+
+namespace or_rviz
+{
+
+
+    class KinBodyDisplay : public rviz::Display
+    {
+        public:
+            KinBodyDisplay();
+            KinBodyDisplay(OpenRAVE::KinBodyPtr kinBody, Ogre::SceneManager* sceneManager);
+            virtual ~KinBodyDisplay();
+
+            void CreateProperties(rviz::Property *parent);
+            void CreateVisual(OpenRAVE::KinBodyPtr kinBody, Ogre::SceneManager* sceneManager);
+            inline void UpdateTransforms() { m_visual->UpdateTransforms(); }
+
+            virtual void onInitialize();
+            virtual void fixedFrameChanged();
+            virtual void reset();
+            virtual void createProperties();
+
+        protected:
+            OpenRAVE::KinBodyPtr m_kinbody;
+            rviz::BoolProperty* m_visibleProperty;
+            KinBodyVisual* m_visual;
+
+    };
+
+} /* namespace superviewer */
+#endif /* KINBODYDISPLAY_H_ */
---
+++ include/or_interactivemarker/rviz/LinkVisual.h
@@ -0,0 +1,73 @@
+/*
+ * LinkVisual.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: mklingen
+ */
+
+#ifndef LINKVISUAL_H_
+#define LINKVISUAL_H_
+
+#include <rviz/properties/property.h>
+#include <openrave/openrave.h>
+#include <openrave/kinbody.h>
+#include <vector>
+#include "Ogre.h"
+
+namespace or_rviz
+{
+    class KinBodyVisual;
+    class LinkVisual
+    {
+        public:
+
+            enum RenderMode
+            {
+                CollisionMesh,
+                VisualMesh
+            };
+
+            LinkVisual(KinBodyVisual* kinBody, OpenRAVE::KinBody::LinkPtr link, Ogre::SceneNode* parent, Ogre::SceneManager* sceneManager);
+            virtual ~LinkVisual();
+
+            virtual void CreateProperties(rviz::Property *parent);
+
+            inline OpenRAVE::KinBody::LinkPtr GetLink() { return m_link.lock(); }
+            inline void SetLink(OpenRAVE::KinBody::LinkPtr value) { m_link = value; }
+
+            inline Ogre::SceneNode* GetSceneNode() { return m_sceneNode; }
+            inline void SetSceneNode(Ogre::SceneNode* value) { m_sceneNode = value; }
+
+            inline KinBodyVisual* GetKinBody() { return m_kinBody; }
+            inline void SetKinBody(KinBodyVisual* value) { m_kinBody = value; }
+            Ogre::MeshPtr meshToOgre(const OpenRAVE::TriMesh& trimesh, std::string name);
+            std::string getMeshName(std::string const filename) const;
+
+
+            inline void SetRenderMode(RenderMode mode) { m_renderMode = mode; CreateParts(); }
+            inline RenderMode GetRenderMode() { return m_renderMode; }
+
+            void CreateParts();
+
+        protected:
+
+            void CreateGeometry(OpenRAVE::KinBody::Link::GeometryPtr geom);
+            void CreateRenderMesh();
+            void LoadRenderMesh(std::string& fileName,
+			OpenRAVE::KinBody::Link::GeometryPtr& geom, Ogre::Entity*& entity,
+			std::string& objectName, std::stringstream& idString,
+			Ogre::Vector3& scale);
+            void CreateCollisionGeometry(OpenRAVE::KinBody::Link::GeometryPtr& geom, Ogre::Entity*& entity, std::string& objectName, std::stringstream& idString, Ogre::Vector3& scale, Ogre::Quaternion& offset_orientation, std::string fileName);
+            void CreateMaterial(std::string& objectName, std::stringstream& idString, OpenRAVE::KinBody::Link::GeometryPtr geom);
+
+            KinBodyVisual* m_kinBody;
+            OpenRAVE::KinBody::LinkWeakPtr m_link;
+            Ogre::SceneNode* m_sceneNode;
+            Ogre::SceneNode* m_parentNode;
+            Ogre::SceneManager* m_sceneManager;
+            RenderMode m_renderMode;
+
+    };
+
+} /* namespace superviewer */
+#endif /* LINKVISUAL_H_ */
