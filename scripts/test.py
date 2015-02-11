295e7454728573b1669d6c175a611de49acd42fd
Michael Koval <mkoval@cs.cmu.edu>
2015-02-11 16:13:46 -0500

    Updated the test.py script.

--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 import openravepy
-
-robot_uri = '/opt/pr/herb_description/ordata/robots/herb.robot.xml'
-#robot_uri = 'robots/barrettwam.robot.xml'
-#robot_uri = '/home/mkoval/backup/ros/r2py/ordata/robots/r2.robot.xml'
+import IPython
 
 env = openravepy.Environment()
-env.Load('/usr/share/openrave-0.9/data/wamtest1.env.xml')
+env.Load('data/wamtest1.env.xml')
+env.SetViewer('RViz')
 
-openravepy.misc.InitOpenRAVELogging()
-openravepy.RaveSetDebugLevel(openravepy.DebugLevel.Debug)
-env.SetViewer('InteractiveMarker openrave_interactivemarker')
-
-import IPython; IPython.embed()
+IPython.embed()
